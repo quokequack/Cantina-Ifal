88fdbc2dba0cfe90317a7895ee5796153a6f8bf0
Malu Alaquoke <88796071+quokequack@users.noreply.github.com>
2022-05-23 18:11:12 -0300

    Completed the code

--- Cantina/Main.java
+++ Cantina/Main.java
@@ -1,8 +1,7 @@
-
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String args[]){
+public class Main {
+    public static void main(String args[]) throws PrecoInvalidoException, QuantidadeInvalidaException{
         //abre um novo estoque
         Estoque estoqueCantina = new Estoque();
         
@@ -13,11 +12,15 @@ public class Main{
         Produto torta = new Produto(004,"Torta", "Salgada", 3.00, 6.00, 4);
         Produto refrigerante = new Produto(005,"Refrigerante", "Guaraná 100Ml", 1.80, 3.50, 100);
         //menu da cantina
-        System.out.println("\n === Cantina Do Ifal ===\nO que deseja fazer?\n1 - Cadastrar produtos;\n2 - Ver produtos em estoque;\n3 - Ver relatório de lucro e prejuízo;\n4 - Dar baixa no estoque\n5 - Sair.");
-        Scanner escolha = new Scanner(System.in);
-        int opcaoEscolhida = escolha.nextInt();
-        while(opcaoEscolhida != 5){
+        int contador = 0;
+        while (contador != 5){
+            System.out.println("\n === Cantina Do Ifal ===\nO que deseja fazer?\n1 - Cadastrar produtos;\n2 - Ver produtos em estoque;\n3 - Ver relatório de lucro e prejuízo;\n4 - Dar baixa no estoque\n5 - Sair.");
+            Scanner escolha = new Scanner(System.in);
+            int opcaoEscolhida = escolha.nextInt();
             if(opcaoEscolhida == 1){
+                System.out.println("Insira o código do produto que deseja cadastrar: ");
+                Scanner codigo = new Scanner(System.in);
+                int novoProdutoCodigo = codigo.nextInt();
                 System.out.println("Insira o nome do produto que quer cadastrar: ");
                 Scanner nome = new Scanner(System.in);
                 String novoProdutoNome = nome.nextLine();
@@ -33,7 +36,7 @@ public class Main{
                 System.out.println("Insira a quantidade comprada: ");
                 Scanner qtdd = new Scanner(System.in);
                 int novoProdutoQuantidade = qtdd.nextInt();
-                estoqueCantina.cadastraProduto(novoProdutoNome, novoProdutoDesc, novoProdutoCompra, novoProdutoVenda, novoProdutoQuantidade);
+                estoqueCantina.cadastraProduto(novoProdutoCodigo, novoProdutoNome, novoProdutoDesc, novoProdutoCompra, novoProdutoVenda, novoProdutoQuantidade);
 
             }else if(opcaoEscolhida == 2){
                 System.out.println("Deseja ver:\n 1 - Produtos disponíveis em estoque por descrição;\n 2 - Produtos disponíveis em estoque por quantidade;\n 3 - Produtos com menos de 50 itens;");
@@ -44,7 +47,7 @@ public class Main{
                 }else if(opcaoEscolhida2 == 2){
                     estoqueCantina.produtosPorQuantidade();
                 }else if(opcaoEscolhida2 == 3){
-                    System.out.println(estoqueCantina.estoquePequeno);
+                    estoqueCantina.produtosEstoquePequeno();
                 }
             }else if(opcaoEscolhida == 3){
                 System.out.println("Deseja ver:\n 1 - Relatório de lucratividade;\n 2 - Relatório de prejuízo.");
@@ -63,11 +66,11 @@ public class Main{
                 System.out.println("Quantos você quer dar baixa? ");
                 Scanner quantidade = new Scanner(System.in);
                 int quantidadeBaixa = quantidade.nextInt();
-                estoqueCantina.darBaixa(quantidadeBaixa,  );
-                //problema: preciso dar um jeito de tirar um objeto da lista passado como parâmetro;
-                //problema: preciso criar o método ...
+                estoqueCantina.darBaixa(quantidadeBaixa, produtoParaBaixa);
+
             }else if(opcaoEscolhida == 5){
                 System.out.println("Até a próxima!");
+                break;
             }
 
         }
