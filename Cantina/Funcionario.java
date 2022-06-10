cacb98b8f5cd447d432d7a7d9c93cf31eb9e9ef6
Malu Alaquoke <88796071+quokequack@users.noreply.github.com>
2022-06-10 18:22:03 -0300

    Create Funcionario.java

---
+++ Cantina/Funcionario.java
@@ -0,0 +1,16 @@
+public class Funcionario{
+    private int login;
+    private String username;
+    private String senha;
+
+
+    public int getLogin(){
+        return this.login;
+    }
+    public String getUsername(){
+        return this.username;
+    }
+    public String getSenha(){
+        return this.senha;
+    }
+}
