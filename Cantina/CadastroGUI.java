5236bff45b89f448e8376f112982028f5b3c733f
Malu Alaquoke <88796071+quokequack@users.noreply.github.com>
2022-06-13 12:19:01 -0300

    Update CadastroGUI.java

--- Cantina/CadastroGUI.java
+++ Cantina/CadastroGUI.java
@@ -1,6 +1,9 @@
+import javax.swing.*;
 import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.*;
 
-public class CadastroGUI extends JFrame {
+public class CadastroGUI extends JFrame implements ActionListener {
     private JLabel lblText;
     private JLabel lblText2;
     private JLabel lblLogin;
@@ -11,12 +14,12 @@ public class CadastroGUI extends JFrame {
     private JTextField txtSenha;
     private Container container;
     private JButton cadastrar;
-    private JButton voltar;
+    private JButton cancelar;
     private JButton limparTudo;
 
 
     public CadastroGUI(){
-        setSize(500,400);
+        setSize(400,400);
         setTitle("Cantina IFAL - Cadastro de funcionarios");
         lblText = new JLabel("CADASTRO DE FUNCIONARIOS");
         lblText2 = new JLabel("Cantina IFAL");
@@ -31,26 +34,41 @@ public class CadastroGUI extends JFrame {
         lblSenha = new JLabel("SENHA:");
         txtSenha = new JTextField();
         cadastrar = new JButton("CADASTRAR");
-        voltar = new JButton("VOLTAR");
-        limparTudo = new JButton("LIMPAR TUDO");
-        lblText.setBounds(80, 10, 200, 90);
-        lblText.setBounds(80, 15, 200, 90);
-        lblLogin.setBounds(40, 30, 100, 25);
-        txtLogin.setBounds(40, 60, 100, 25);
-        lblNome.setBounds(40, 90, 100, 25);
-        txtNome.setBounds(40, 120, 100, 25);
-        lblSenha.setBounds(40, 150, 100, 25);
-        txtSenha.setBounds(40, 180, 100, 25);
+        cancelar = new JButton("VOLTAR");
+        limparTudo = new JButton("LIMPAR");
+        lblText.setBounds(110, 0, 200, 25);
+        lblText2.setBounds(160, 15, 150,25);
+        lblLogin.setBounds(95, 45, 200,25);
+        txtLogin.setBounds(95, 75, 200, 25);
+        lblNome.setBounds(95, 105, 200, 25);
+        txtNome.setBounds(95, 135, 200, 25);
+        lblSenha.setBounds(95, 165, 200, 25);
+        txtSenha.setBounds(95, 195, 200, 25);
+        limparTudo.setBounds(95, 255, 100, 25);
+        cancelar.setBounds(195, 255, 100, 25);
+        cadastrar.setBounds(95,285, 200, 25);
         container.add(lblText);
+        container.add(lblText2);
         container.add(txtLogin);
         container.add(lblLogin);
         container.add(lblNome);
         container.add(txtNome);
         container.add(lblSenha);
         container.add(txtSenha);
+        container.add(cadastrar);
+        container.add(cancelar);
+        container.add(limparTudo);
+        cadastrar.addActionListener(this);
     }
     public static void main(String args[]){
         CadastroGUI cadastroTeste = new CadastroGUI();
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("Cadastrar")){
+            
+        }
+        
     }
 
 }
