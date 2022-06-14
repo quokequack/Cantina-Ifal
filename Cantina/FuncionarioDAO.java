72f511426120b5cd32f957d21c036250cf94bb9d
Malu Alaquoke <88796071+quokequack@users.noreply.github.com>
2022-06-13 23:01:12 -0300

    ADDED VALIDALOGIN METHOD

--- Cantina/FuncionarioDAO.java
+++ Cantina/FuncionarioDAO.java
@@ -13,13 +13,30 @@ public class FuncionarioDAO {
         conexao = FabricaCon.criaConexao();
     }
 
+    String sql2 = "select login from funcionario where login=?";
+        
+
+    public boolean validaLogin(String login) throws SQLException{
+        try(
+            PreparedStatement stmt2 = conexao.prepareStatement(sql2);
+        ){
+            stmt2.setString(1, login);
+            try (ResultSet resultado = stmt2.executeQuery()){
+                return resultado.next();
+            }
+        }
+    }
     public void adiciona(Funcionario funcionario){
-        String sql = "insert into funcionario(login, nome, senha)"+"values(?,?,?)";
         try{
+            String sql = "insert into funcionario(login, nomefuncionario, senha)"+"values(?,?,?)";
             PreparedStatement stmt = conexao.prepareStatement(sql);
-            stmt.setInt(1, funcionario.getLogin());
+            stmt.setString(1, funcionario.getLogin());
             stmt.setString(2, funcionario.getUsername());
             stmt.setString(3, funcionario.getSenha());
+            stmt.execute();
+
+            //adicionar aqui uma estrutura condicional
+
         }catch(SQLException e){
             throw new RuntimeException("Não foi possível adicionar. Erro: "+e);
         }
