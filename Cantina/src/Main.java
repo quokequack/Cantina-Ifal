5872fb0b891b4ad0eb280418891fb981a9c6f7e2
Malu Alaquoke <88796071+quokequack@users.noreply.github.com>
2022-06-15 09:51:33 -0300

    Add files via upload

--- Cantina/src/Main.java
+++ Cantina/src/Main.java
@@ -1,8 +1,6 @@
 import java.sql.SQLException;
 import java.util.Scanner;
 
-import javax.sound.midi.MidiEvent;
-
 public class Main {
     public static void main(String args[]) throws PrecoInvalidoException, QuantidadeInvalidaException, SQLException{
         //abre um novo estoque
--- Cantina/src/ProdutoDAO.java
+++ Cantina/src/ProdutoDAO.java
@@ -107,10 +107,7 @@ public class ProdutoDAO {
             System.out.println("Não foi possível realizar a consulta. Erro: "+e);
         }
     }
-    public void mudaPrecoCompra(int codprod){
-        String updateCompra = "update produto set precocompra = ?";
-        PreparedStatement stmt5
-        //método que faz uma pesquisa no banco de dados e retorna os produtos com estoque pequeno
+    //método que faz uma pesquisa no banco de dados e retorna os produtos com estoque pequeno
     public void estoquePeq(){
         try{
             String selectQtddMin = "select nomeprod, qtdddisponivel from produto where qtdddisponivel <= 50 order by qtdddisponivel";
